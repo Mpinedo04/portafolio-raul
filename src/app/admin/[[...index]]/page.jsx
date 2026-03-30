98f1909cebcf5b777e52c9fea1725f0726353082
Mpinedo04 <93118093+Mpinedo04@users.noreply.github.com>
2026-03-30 03:20:13 +0200

    Arreglo: Añadida la ruta /admin para cargar Sanity Studio

---
+++ src/app/admin/[[...index]]/page.jsx
@@ -0,0 +1,11 @@
+'use client';
+import { NextStudio } from 'next-sanity/studio';
+import config from '../../../../sanity.config';
+
+export default function AdminPage() {
+  return (
+    <div style={{ minHeight: '100vh', width: '100%', paddingTop: '80px' }}>
+      <NextStudio config={config} />
+    </div>
+  );
+}
